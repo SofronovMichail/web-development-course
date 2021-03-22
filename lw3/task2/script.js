8e14faa49cc6f916dca48ee3d34427b4ff021192
SofronovMichail <77967254+SofronovMichail@users.noreply.github.com>
2021-03-22 20:33:24 +0300

    Updated input verification feature

--- lw3/task2/script.js
+++ lw3/task2/script.js
@@ -1,10 +1,5 @@
 function isTimeRangesIntersect(timeRange1, timeRange2) {
-  if (
-    timeRange1 !== null &&
-    typeof timeRange1 === "object" &&
-    timeRange2 !== null &&
-    typeof timeRange2 === "object"
-  ) {
+  if (Array.isArray(timeRange1) && Array.isArray(timeRange2)) {
     console.log("Time range first: " + timeRange1.join(" - "));
     console.log("Time range second: " + timeRange2.join(" - "));
     let result = timeRange1[1] >= timeRange2[0];
