d33b89e8372cc92d4d83bb9d0edea083d0d3d183
SofronovMichail <77967254+SofronovMichail@users.noreply.github.com>
2021-03-23 11:19:36 +0300

    Update script.js

--- lw3/task2/script.js
+++ lw3/task2/script.js
@@ -1,10 +1,8 @@
 function isTimeRangesIntersect(timeRange1, timeRange2) {
   if (Array.isArray(timeRange1) && Array.isArray(timeRange2)) {
-    console.log("Time range first: " + timeRange1.join(" - "));
-    console.log("Time range second: " + timeRange2.join(" - "));
     let result = timeRange1[1] >= timeRange2[0];
     return result;
   } else {
-    console.log("Неверный формат данных");
+    return false;
   }
 }
