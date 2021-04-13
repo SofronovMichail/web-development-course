43e358f296bd7156e73977582e023bf8c9d5befd
SofronovMichail <77967254+SofronovMichail@users.noreply.github.com>
2021-04-13 22:58:10 +0300

    Complete task4

--- lw4/task4/script.js
+++ lw4/task4/script.js
@@ -4,7 +4,7 @@ String.prototype.countCoincidence = function (arrElement) {
   while ((position = this.indexOf(arrElement, position + 1)) != -1) {
     count++;
   }
-  console.log(count);
+  return count;
 };
 
 "Hello world".countCoincidence("Hello"); // must return 1
@@ -12,12 +12,3 @@ String.prototype.countCoincidence = function (arrElement) {
 "Hello world".countCoincidence("aaa"); // must return 0
 
 "Hello world".countCoincidence("o"); // must return 2
-
-// let str = "Hello world";
-// let target = "Hello";
-// let count = 0;
-// let pos = -1;
-// while ((pos = str.indexOf(target, pos + 1)) != -1) {
-//   count++;
-// }
-// console.log("Score: " + count);
