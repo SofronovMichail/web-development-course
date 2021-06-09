263491f239e26f055d975a811259fef8beafb6b2
SofronovMichail <77967254+SofronovMichail@users.noreply.github.com>
2021-06-09 14:45:45 +0300

    Create test for lw5

--- lw5/task2/listing-store.test.js
+++ lw5/task2/listing-store.test.js
@@ -1,5 +1,26 @@
-const sum = require('./listing-store');
+import {
+  listingElements,
+  storeElements,
+  addNewElements,
+  addToStoreElements,
+} from './listing-store';
 
-test('storeElements = []', () => {
-  expect(storeElements).toBe([]);
+describe('add new elements and switch them', () => {
+  test('addNewElements should add new element', () => {
+    addNewElements('something');
+    expect(listingElements).toContain('something');
+    expect(listingElements.length).toBe(3);
+  });
+
+  test('addToStoreElements should add dedicated element to storeElements', () => {
+    addToStoreElements('apple');
+    expect(storeElements).toContain('apple');
+    expect(storeElements.length).toBe(1);
+  });
+
+  test('addToStoreElements should add dedicated element to storeElements', () => {
+    addToListingElements('apple');
+    expect(listingElements).toContain('apple');
+    expect(listingElements.length).toBe(2);
+  });
 });
